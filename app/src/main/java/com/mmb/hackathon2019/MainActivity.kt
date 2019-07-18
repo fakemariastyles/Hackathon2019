1ab0ec4686e6385d71d6f72f0c0aa9f3299a0ab0
fakemariastyles <mary.mirzayee@gmail.com>
2019-07-18 13:29:20 +0430

    commit for pull

--- app/src/main/java/com/mmb/hackathon2019/MainActivity.kt
+++ app/src/main/java/com/mmb/hackathon2019/MainActivity.kt
@@ -2,9 +2,12 @@ package com.mmb.hackathon2019
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mmb.hackathon2019.data.repository.HackathonRepository
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-
+    @Inject
+    lateinit var hackathonRepository:HackathonRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/mmb/hackathon2019/application/ui/ResultFragment.kt
@@ -0,0 +1,7 @@
+package com.mmb.hackathon2019.application.ui
+
+import androidx.fragment.app.Fragment
+
+class ResultFragment : Fragment(){
+
+}
