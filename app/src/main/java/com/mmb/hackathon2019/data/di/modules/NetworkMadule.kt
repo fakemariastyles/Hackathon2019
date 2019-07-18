9565c7da33f1cb04949fe28a13454b2d5e2ba18f
fakemariastyles <mary.mirzayee@gmail.com>
2019-07-18 12:45:42 +0430

    network module added

---
+++ app/src/main/java/com/mmb/hackathon2019/data/di/modules/NetworkMadule.kt
@@ -0,0 +1,47 @@
+package com.mmb.hackathon2019.data.di.modules
+
+import com.mmb.hackathon2019.R
+import com.mmb.hackathon2019.remote.api.NetworkManager
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkMadule{
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit{
+        return  Retrofit.Builder()
+            .client(okHttpClient)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(NetworkManager.BASE_URL)
+            .build()
+    }
+
+    @Provides
+    fun provideOkHttpClient():OkHttpClient{
+        return OkHttpClient()
+            .newBuilder()
+            .addInterceptor { chain ->
+                val request = chain.request()
+                val requestBuilder = request.newBuilder()
+                    .header("Authorization", R.string.access_token.toString())
+
+                val newRequest = requestBuilder.build()
+                chain.proceed(newRequest)
+            }
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNetworkManager(retrofit: Retrofit):NetworkManager{
+        return retrofit.create(NetworkManager::class.java)
+    }
+
+
+}
--- app/src/main/java/com/mmb/hackathon2019/remote/datasource/HackathonRemoteDataSource.kt
+++ app/src/main/java/com/mmb/hackathon2019/remote/datasource/HackathonRemoteDataSource.kt
@@ -5,8 +5,8 @@ import com.mmb.hackathon2019.remote.dto.ResultDto
 import io.reactivex.Single
 import javax.inject.Inject
 
-class HackathonRemoteDataSource @Inject constructor(private val networkManager: NetworkManager){
-    fun getBalance(serial: String): Single<ResultDto>{
+class HackathonRemoteDataSource @Inject constructor(private val networkManager: NetworkManager) {
+    fun getBalance(serial: String): Single<ResultDto> {
         return networkManager.getBalance(serial)
     }
 }
